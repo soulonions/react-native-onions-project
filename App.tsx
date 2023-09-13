6b0b1cb0aa24231057297d2c3660ff81d1f1ceb5
Hoang Anh <lonbietbay3198@gmail.com>
2023-09-13 16:26:30 +0700

    init repo
    init repo

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './src/home/home.screen';
+
+function App(): JSX.Element {
+  return <Home />;
+}
+
+export default App;
---
+++ src/home/home.screen.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Animated, {
+  Easing,
+  ReduceMotion,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import {Button, Colors, View} from 'react-native-ui-lib';
+
+const Home = () => {
+  const widthViewAnimated = useSharedValue(100);
+  const offsetViewAmimated = useSharedValue(50);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: offsetViewAmimated.value}],
+  }));
+
+  const handlePress = () => {
+    widthViewAnimated.value = withSpring(widthViewAnimated.value + 10);
+  };
+
+  React.useEffect(() => {
+    offsetViewAmimated.value = withRepeat(
+      withTiming(-offsetViewAmimated.value, {
+        duration: 1500,
+        easing: Easing.bounce,
+        reduceMotion: ReduceMotion.System,
+      }),
+      -1,
+      true,
+    );
+  }, []);
+
+  return (
+    <View flex backgroundColor={Colors.grey70}>
+      <View flex backgroundColor="white" center>
+        <Animated.View
+          style={[
+            {
+              width: widthViewAnimated,
+              height: 100,
+              backgroundColor: 'violet',
+            },
+            animatedStyle,
+          ]}
+        />
+      </View>
+      <View center marginV-15>
+        <Button onPress={handlePress} />
+      </View>
+    </View>
+  );
+};
+
+export default Home;
